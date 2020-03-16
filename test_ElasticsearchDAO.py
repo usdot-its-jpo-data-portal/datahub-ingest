75d92429142a7896a51850a3b612aacdb3c7bd65
mvs5465 <mvs5465@gmail.com>
2020-03-16 11:58:44 -0400

    Finalize cicd files

--- test_ElasticsearchDAO.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from ElasticsearchDAO import ElasticsearchDAO
-
-class TestElasticsearchDAO(unittest.TestCase):
-    def test_writeToElasticsearch_nodata(self):
-        test_es_dao = ElasticsearchDAO()
-        test_es_dao.writeToElasticsearch([])
